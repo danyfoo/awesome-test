cd328a4f2240f8807330f3f9c4c18f7897df08d7
Daniel Rivera <drivera1@uabc.edu.mx>
2016-03-13 14:06:27 -0700

    HTTP Server, Express Server Examples

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log('ITS MY FIRST NODE APP! OH AND I GUESS HELLO WORLD');
+/*
+HTTP SERVER
+
+var http = require('http'),
+    fs = require('fs');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {
+        'Content-Type' : 'text/html',
+        'Access-Control-Allow-Origin' : '*'
+    });
+
+    var readStream = fs.createReadStream(__dirname + '/index.html');
+
+    readStream.pipe(res);
+}).listen(3000);
+
+console.log('Visit me at http://localhost:3000');
+*/
+
+var express = require('express'),
+    app = express(),
+    path = require('path');
+
+app.get('/', function(req, res){
+    "use strict";
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
+console.log('Visit me at http://localhost:3000');
